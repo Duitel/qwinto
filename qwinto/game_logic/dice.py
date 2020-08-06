1d450e41626af2dff273292312bcaeda18de6f76
Duitel <jop.j@hotmail.com>
2020-08-06 14:25:06 +0200

    Class for single dice.

--- qwinto/game_logic/dice.py
+++ qwinto/game_logic/dice.py
@@ -1,1 +1,12 @@
-pass
+from random import randint
+
+class Dice:
+	def __init__(color: str):
+		this.color = color
+
+	def throw() -> int:
+		"""Throw the dice.
+
+		:returns: int, a random number between 1 and 6
+		"""
+		return randint(1,6)
